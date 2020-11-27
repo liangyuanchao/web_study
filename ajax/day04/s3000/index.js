bf83ca4b73cb585c48959f56b95b95d1b1c0512a
梁远朝 <1661277636@qq.com>
2020-11-27 22:33:27 +0800

    跨域

--- ajax/day04/s3000/index.js
+++ ajax/day04/s3000/index.js
@@ -1,17 +1,40 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const static = require('koa-static');
+const axios = require('axios');
+
+// 解决重复输入接口
+const proxy = require('koa-server-http-proxy');
 
 const app = new Koa();
 const router = new Router();
 
 app.use(static(__dirname + '/static'));
+// 配置 proxy，也是一种代理方式
+app.use(
+    proxy("/api",{
+        target: "http://localhost:4000",
+        pathRewrite: {
+            "^/api": "",
+        }
+    }
+))
 
 router.get('/', (ctx, next) => {
     ctx.body = 'Hello, ajax'
 })
 
+// // 7.html
+router.get('/getDaili', async (ctx) => {
+    // 代理：浏览器请求同源服务器端，让服务器端去请求非同源服务器端，绕过了浏览器的同源策略
+    // 需要借助第三方库 axios
+    const res = await axios.get('http://localhost:4000/getGency');
+    ctx.body = res.data;
+});
 
+// router.get('/getData', (ctx) => {
+//     ctx.body = 'ok'
+// })
 
 
 
--- ajax/day04/s4000/index.js
+++ ajax/day04/s4000/index.js
@@ -7,12 +7,12 @@ const router = new Router();
 
 app.use(static(__dirname + '/static'));
 
-router.get('/', (ctx, next) => {
+router.get('/', (ctx) => {
     ctx.body = 'Hello, ajax'
 })
 
 // 01.html
-router.get('/getAjax', (ctx, next) => {
+router.get('/getAjax', (ctx) => {
     ctx.body = {
         name: 'zhangsan',
         age: 20
@@ -20,7 +20,7 @@ router.get('/getAjax', (ctx, next) => {
 })
 
 // 02.html  03.html
-router.get('/better', (ctx, next) => {
+router.get('/better', (ctx) => {
     // 接收客户端传递过来的函数名称
     let cb = ctx.query.callback;
     let obj = {
@@ -32,9 +32,40 @@ router.get('/better', (ctx, next) => {
 })
 
 
-router.get('/cross', (ctx, next) => {
+router.get('/cross', (ctx) => {
     ctx.body = 'ok'
 })
+
+// 05.html
+router.get('/getData', (ctx) => {
+    console.log('/getData');
+    // 任意域名都可以访问
+    // ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.body = 'get - data - 4000'
+})
+
+// 06.html
+router.options("/postData", (ctx) => {
+    ctx.set("Access-Control-Allow-Origin", "*");
+    ctx.set("Access-Control-Allow-Methods", "POST");
+    ctx.set("Access-Control-Allow-Headers", "content-type");
+    ctx.body = "";
+});
+router.post("/postData", (ctx) => {
+    ctx.set("Access-Control-Allow-Origin", "*");
+    ctx.body = "post - data - 4000";
+});
+
+// 07.html
+router.get('/getGency', (ctx) => {
+    ctx.body = 'get - data - 4000';
+})
+
+//08.html
+router.post('/getGency', (ctx) => {
+    ctx.body = 'post - data - 4000';
+})
+
 
 app.use(router.routes());
 
