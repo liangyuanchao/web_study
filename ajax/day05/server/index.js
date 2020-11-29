094ec55e6ce51b35f41e86aa012bbe370a54478f
梁远朝 <1661277636@qq.com>
2020-11-29 23:02:05 +0800

    $.ajax

--- ajax/day05/server/index.js
+++ ajax/day05/server/index.js
@@ -1,16 +1,21 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const static = require('koa-static');
+const koaBody = require('koa-body');
 
 const app = new Koa();
 const router = new Router();
 
 app.use(static(__dirname + '/static'));
+app.use(koaBody({
+    multipart: true,
+}))
 
 router.get('/', ctx => {
     ctx.body = 'Hello - server'
 });
 
+// 01.html
 router.post('/base', ctx => {
     // ctx.status = 404
     ctx.body = {
@@ -18,6 +23,22 @@ router.post('/base', ctx => {
         age: 30
     }
 })
+// 02.html
+router.get('/user', ctx => {
+    ctx.body = ctx.query;
+})
+router.post('/user', ctx => {
+    ctx.body = ctx.body;
+})
+
+// 05.html
+router.get('/jsonp', ctx => {
+    const cd = ctx.body.cd;
+    const data = cd + "({name: 'zhangsan})";
+    ctx.body = data;
+})
+
+
 
 app.use(router.routes());
 
